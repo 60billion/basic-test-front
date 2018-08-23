0ec03bd3c00a9ff2513d463e601bbd3491f90b83
Allreview <60billion@allrvw.com>
2018-08-23 14:11:00 +0900

    realized that I don't need session

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import { Url } from '../url/url';
+import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'page-about',
@@ -7,8 +11,21 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  printing:string;
 
+  constructor(public navCtrl: NavController,
+              public http: Http,
+              public url: Url) {
   }
+
+  _print(){
+    let data = ""
+    this.http.post(this.url.url+"/session",data).pipe(
+      map(res=>res.json())
+    ).subscribe(response=>{
+      this.printing = response.result;
+    })
+  }
+
 
 }
